4952e34ae79a7f430b87da3ec7180f826388c370
Bram <Bram.mathijssen@gmail.com>
2023-05-10 17:47:18 +0200

    feat: added wallet connection

---
+++ src/components/UI/ConnectButton.jsx
@@ -0,0 +1,17 @@
+import { ConnectKitButton } from "connectkit";
+
+const ConnectButton = () => {
+    return (
+        <>
+            <ConnectKitButton
+                className="connectButton"
+                customTheme={{
+                    "--ck-connectbutton-color": "#000000",
+                    "--ck-connectbutton-background": "#27dfe6",
+                }}
+            />
+        </>
+    );
+};
+
+export default ConnectButton;
