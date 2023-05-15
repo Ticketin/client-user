8847db05197c8aba46efe9e0233fe754797a78ab
Bram <Bram.mathijssen@gmail.com>
2023-05-15 09:04:14 +0200

    chore: minor style changes

--- src/components/UI/ConnectButton.jsx
+++ src/components/UI/ConnectButton.jsx
@@ -6,8 +6,8 @@ const ConnectButton = () => {
             <ConnectKitButton
                 className="connectButton"
                 customTheme={{
-                    "--ck-connectbutton-color": "#000000",
-                    "--ck-connectbutton-background": "#27dfe6",
+                    // "--ck-connectbutton-color": "#000000",
+                    // "--ck-connectbutton-background": "#27dfe6",
                 }}
             />
         </>
