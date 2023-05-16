6c380e59ff13d28baef331daf868a8eab027fbb3
Bram <Bram.mathijssen@gmail.com>
2023-05-16 20:43:02 +0200

    feat: added functionality to register new tickets

--- src/components/Admin/RegisterForm.jsx
+++ src/components/Admin/RegisterForm.jsx
@@ -2,25 +2,61 @@ import * as React from "react";
 import { useForm } from "react-hook-form";
 import InputField from "../UI/InputField";
 import Dropdown from "../UI/Dropdown";
+import { ticketCollectionFactoryAbi } from "../../constants";
 
 import styles from "./RegisterForm.module.scss";
+import useDebounce from "../../hooks/useDebounce";
+import { useAccount, useContractWrite, useNetwork, usePrepareContractWrite, useWaitForTransaction } from "wagmi";
 
 const RegisterForm = () => {
+    const { chain } = useNetwork();
+    const { isConnected, address } = useAccount();
+
     const {
         register,
         formState: { errors },
         handleSubmit,
         watch,
     } = useForm({
-        mode: "all", // "onChange" or "onBlur"
+        mode: "all", // "onChange" + "onBlur"
     });
 
-    const watchAddress = watch("address", false); // false is defeault value
-    const watchName = watch("name", false);
-    const watchRole = watch("role", false);
+    const watchEventName = watch("eventName", false); // false is defeault value
+    const eventSymbol = watch("eventSymbol", false);
+
+    // debouncing watched variables so we don't overload the RPC in usePrepareContractWrite
+    const debouncedName = useDebounce(watchEventName);
+    const debouncedSymbol = useDebounce(eventSymbol);
+
+    // prepares the contract write, this way we pre-send our args to the RPC, we can catch errors before we send.
+    // address: we get the right contract address by the currently connected chain
+    // enabled: only enable this hook if isConnected, debouncedAddress, debouncedName and debouncedRole are true
+    // args: all the debounched args, so we only call this hook every 0,5s max, also convert the string to bytes32
+    const {
+        config,
+        error: prepareError,
+        isError: isPrepareError,
+    } = usePrepareContractWrite({
+        address: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+        abi: ticketCollectionFactoryAbi,
+        functionName: "deployNewTicketCollection",
+        enabled: true,
+        args: [debouncedName, debouncedSymbol, address],
+    });
+
+    const { data, error, isError, write: createNewEvent } = useContractWrite(config);
+
+    // wait for the transaction to finish so we can refresh the data on success
+    const { isLoading, isSuccess } = useWaitForTransaction({
+        hash: data?.hash,
+        onSuccess(data) {
+            console.log(`event added!`);
+        },
+    });
 
     const onSubmit = (data) => {
-        alert(JSON.stringify(data));
+        console.log("adding new event");
+        createNewEvent?.();
     };
 
     return (
@@ -29,20 +65,27 @@ const RegisterForm = () => {
                 <div>
                     <InputField
                         type="text"
-                        name="address"
+                        name="eventName"
                         label="Event Name"
                         errors={errors}
                         register={register}
                         validationSchema={{
-                            required: "Address is required.",
-                            pattern: {
-                                value: /^0x[a-fA-F0-9]{40}$/i,
-                                message: "Please enter a valid erc20 address",
-                            },
+                            required: "Event Name is required",
                         }}
                         required
                     />
                     <InputField
+                        type="text"
+                        name="eventSymbol"
+                        label="Event Symbol"
+                        errors={errors}
+                        register={register}
+                        validationSchema={{
+                            required: "Event Symbol is required.",
+                        }}
+                        required
+                    />
+                    {/* <InputField
                         type="text"
                         name="eventDate"
                         label="Event Date"
@@ -53,7 +96,7 @@ const RegisterForm = () => {
                             maxLength: { value: 32, message: "Can't be more than 32 characters" },
                         }}
                         required
-                    />
+                    /> */}
                     <InputField
                         type="number"
                         name="ticketAmount"
@@ -66,7 +109,7 @@ const RegisterForm = () => {
                         }}
                         required
                     />
-                    <InputField
+                    {/* <InputField
                         type="number"
                         name="ticketPrice"
                         label="Single Ticket Price"
@@ -92,11 +135,16 @@ const RegisterForm = () => {
                             required: "Role is required.",
                         }}
                         required
-                    />
+                    /> */}
                 </div>
                 <div className={styles.submitButton}>
-                    <button type="submit">Add Member</button>
+                    <button type="submit">Create Event</button>
                 </div>
+                {isSuccess ? (
+                    <div className={styles.txSuccess}>
+                        <p> ✅ Succesfully added new event!</p>
+                    </div>
+                ) : null}
             </form>
         </>
     );
--- src/components/Admin/RegisterTicket.jsx
+++ src/components/Admin/RegisterTicket.jsx
@@ -1,15 +1,13 @@
+import ConnectButton from "../UI/ConnectButton";
 import RegisterForm from "./RegisterForm";
 import styles from "./RegisterTicket.module.scss";
 
 const RegisterTicket = () => {
     return (
         <div className={styles.registerTicketContainer}>
-            <div className={styles.gridContainer}>
-                <div className={styles.formContainer}>
-                    <h2>Register new Ticket</h2>
-                    <RegisterForm />
-                </div>
-                <div className={styles.resultContainer}></div>
+            <div className={styles.formContainer}>
+                <h2>Register new Event</h2>
+                <RegisterForm />
             </div>
         </div>
     );
