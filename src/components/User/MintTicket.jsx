118d720224e15c25eafd7107422aacb4cb24ce38
Bram <Bram.mathijssen@gmail.com>
2023-05-20 20:25:42 +0200

    feat: added functionality to mint tickets and return values from hardcoded tokenURI

--- src/components/User/MintTicket.jsx
+++ src/components/User/MintTicket.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import { usePrepareContractWrite, useContractWrite, useWaitForTransaction, useAccount, useContractRead, useContractEvent, useContractReads } from "wagmi";
 import { ticketCollectionFactoryAbi, ticketCollectionAbi } from "../../constants";
 import { useState } from "react";
-
+import nbaCardImage from "./../../assets/images/nba-card2.png";
+import nbaLogo from "./../../assets/images/nba-logo.png";
 import styles from "./MintTicket.module.scss";
+import FlipCard, { BackCard, FrontCard } from "../UI/FlipCard";
 
 const MintTicket = () => {
     const { isConnected, address } = useAccount();
@@ -87,35 +89,59 @@ const MintTicket = () => {
     };
 
     return (
-        <div className="userMintSection">
-            <div className={styles.titleWrapper}>
-                <h2>USER SECTION</h2>
-                <div className={styles.flexContainer}>
-                    <div className={styles.mintButtonContainer}>
-                        {ownedNfts && <p>{ownedNfts.toString()}</p>}
-                        <button onClick={handleMintNft}>Mint Ticket</button>
-                    </div>
-                    <div className={styles.mintedTicketContainer}>
-                        {isSuccessMinftNft && tokenURI && nftData ? (
-                            <div className={styles.mintedTicketData}>
-                                <div className={styles.ticketDetails}>
-                                    <p>{nftData.name}</p>
-                                    <p>{nftData.description}</p>
-                                    {/* <p>{tokenURI}</p> */}
-                                </div>
-                                <div className={styles.ticketImage}>
-                                    <img src={nftData.image} alt="qr-code"></img>
-                                </div>
+        <>
+            <div className="userMintSection">
+                <div className={styles.titleWrapper}>
+                    <h2>USER SECTION</h2>
+                    <div className={styles.flexContainer}>
+                        <div className={styles.mintTicketContainer}>
+                            <h2>NBA All-Stars Tickets</h2>
+                            {ownedNfts && <p>Currently owned tickets: {ownedNfts.toString()}</p>}
+                            <div className={styles.mintButtonContainer}>
+                                <p>price: 0,01 ETH </p>
+                                <button onClick={handleMintNft}>Mint Ticket</button>
                             </div>
-                        ) : null}
+                        </div>
+                        <div className={styles.mintedTicketContainer}>
+                            {/* <img className={styles.frontCardImage} src={nbaLogo} alt="RainbowKit Demo NFT" /> */}
+                            <div className={styles.testSection}>
+                                <FlipCard>
+                                    <FrontCard isCardFlipped={isSuccessMinftNft}>
+                                        <img className={styles.frontCardImage} src={nbaLogo} alt="RainbowKit Demo NFT" />
+                                        <h3 style={{ marginTop: 24 }}>NBA All-Stars</h3>
+                                    </FrontCard>
+                                    <BackCard isCardFlipped={isSuccessMinftNft}>
+                                        {nftData ? (
+                                            <div className={styles.mintedTicketData}>
+                                                <div className={styles.ticketDetails}>
+                                                    <div className={styles.mainContent}>
+                                                        <img src={nbaLogo} width="120" alt="RainbowKit Demo NFT" style={{ borderRadius: 8 }} />
+                                                        <p>{nftData.name}</p>
+                                                        <p>{nftData.description}</p>
+                                                    </div>
+                                                    <div className={styles.footerContent}>
+                                                        <p>
+                                                            View on <a href={`https://rinkeby.etherscan.io/tx/`}>Etherscan</a>
+                                                        </p>
+                                                        <p>
+                                                            View on <a href={`https://testnets.opensea.io/assets/rinkeby/1`}>Opensea</a>
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                                <div className={styles.ticketImage}>
+                                                    <img src={nftData.image} alt="qr-code"></img>
+                                                </div>
+                                            </div>
+                                        ) : null}
+                                    </BackCard>
+                                </FlipCard>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
 export default MintTicket;
-
-
-
