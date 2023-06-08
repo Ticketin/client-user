b5664d84eedadb3ae853cafc9f22c2d8217888a2
Bram <Bram.mathijssen@gmail.com>
2023-06-08 04:39:53 +0200

    build: updated dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 // build
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    // in case you want to interact with rollup you can use
+  plugins: [svgr(), react()],
+  // in case you want to interact with rollup you can use
 
-    optimizeDeps: {
-        exclude: ["nft.storage"],
+  optimizeDeps: {
+    exclude: ["nft.storage"],
+  },
+  build: {
+    rollupOptions: {
+      external: [
+        "nft.storage", // ignore nft.storage
+      ],
     },
-    build: {
-        rollupOptions: {
-            external: [
-                "nft.storage", // ignore nft.storage
-            ],
-        },
-    },
+  },
 });
